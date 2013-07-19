996cd1fa8c4f7a6a8f28534ba06731bedf21bbf3
lpararasasingam <laxamnn@hotmail.com>
2013-07-19 00:56:37 +0000

    Styled html page

--- web.js
+++ web.js
@@ -4,10 +4,10 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('./index.html').toString('utf-8'));
+  response.send(fs.readFileSync('./index2.html').toString('utf-8'));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
